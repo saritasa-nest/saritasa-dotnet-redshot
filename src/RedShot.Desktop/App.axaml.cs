172337c615c5d283e3192a488e00e7afa0eb5f09
Ivan Kozhin <ivan@saritasa.com>
2020-07-13 16:56:11 +0700

    Initial commit #SN-46

---
+++ src/RedShot.Desktop/App.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using RedShot.Desktop.ViewModels;
+using RedShot.Desktop.Views;
+
+namespace RedShot.Desktop
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
