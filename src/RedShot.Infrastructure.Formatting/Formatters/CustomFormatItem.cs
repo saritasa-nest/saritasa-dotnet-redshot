232c297d68786cab86d5eb636350f2d9b69cfee2
Vladimir Rudt <vladimir.rudt@saritasa.com>
2020-10-30 20:14:52 +0700

    Add custom format item #SN-54

---
+++ src/RedShot.Infrastructure.Formatting/Formatters/CustomFormatItem.cs
@@ -0,0 +1,17 @@
+﻿namespace RedShot.Infrastructure.Formatting.Formatters
+{
+    /// <summary>
+    /// Custom format
+    /// </summary>
+    internal class CustomFormatItem : IFormatItem
+    {
+        /// <inheritdoc/>
+        public string Name => "Custom";
+
+        /// <inheritdoc/>
+        public string Pattern => "[your_text]";
+
+        /// <inheritdoc/>
+        public string GetText() => "your_text";
+    }
+}
