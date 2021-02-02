b46fc45e5b6f6e352bc38b25638d1c8323decd7f
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-02-03 00:12:53 +0700

    Remake recording view, make fade on recording #SN-59

--- src/RedShot.Infrastructure.Recording/Views/RecordingView.cs
+++ src/RedShot.Infrastructure.Recording/Views/RecordingView.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
 using Eto.Drawing;
 using Eto.Forms;
 using RedShot.Infrastructure.Common;
+using RedShot.Infrastructure.Common.Forms;
 using RedShot.Infrastructure.Recording.Abstractions;
+using RedShot.Infrastructure.Uploading;
 
 namespace RedShot.Infrastructure.Recording.Views
 {
     /// <summary>
     /// Recording view.
     /// </summary>
-    internal class RecordingView : Form
+    internal partial class RecordingView : Form
     {
         private readonly IRecordingService recordingService;
         private readonly Rectangle recordingRectangle;
+        private UITimer renderingTimer;
+        private Stopwatch recordingTimer;
+        private RecordingButton recordingButton;
+        private ImageButton closeButton;
+        private ImageButton optionsButton;
+        private Label timerLabel;
+        private IRecorder recorder;
+        private Size optionsPanelSize;
 
         /// <summary>
         /// Initializes recording view.
@@ -25,60 +36,104 @@ namespace RedShot.Infrastructure.Recording.Views
             Resizable = false;
 
             InitializeComponents();
+            this.UnLoad += (o, e) => recorder?.Stop();
         }
 
-        private void InitializeComponents()
+        private void RecordingButtonClicked(object sender, System.EventArgs e)
         {
-            var recordingVideoPanel = new RecordingVideoPanel(recordingService, recordingRectangle);
-            recordingVideoPanel.Closed += RecordingVideoPanelClosed;
-
-            Content = new StackLayout()
+            recordingButton.Enabled = false;
+            if (recordingButton.IsRecording)
             {
-                Orientation = Orientation.Vertical,
-                Items =
-                {
-                    recordingVideoPanel
-                }
-            };
-
-#if _WINDOWS
-            recordingVideoPanel.BackgroundColor = Colors.WhiteSmoke;
-            BackgroundColor = Colors.Red;
-            this.MovableByWindowBackground = false;
-
-            WindowStyle = WindowStyle.None;
-            BackgroundColor = Colors.Red;
-
-            Rectangle excludeRectangle = default;
-            Rectangle excludeRectangle2 = default;
-
-            if ((recordingRectangle.Location.Y - recordingVideoPanel.Height) > 0)
-            {
-                Location = new Point(recordingRectangle.X, recordingRectangle.Y - recordingVideoPanel.Height - 1);
-                Size = new Size(recordingRectangle.Width, recordingRectangle.Height + recordingVideoPanel.Height + 1);
-                excludeRectangle = new Rectangle(new Point(0, recordingVideoPanel.Height), new Size(recordingRectangle.Width, recordingRectangle.Height + 1)).OffsetRectangle(1);
-                excludeRectangle2 = new Rectangle(new Point(recordingVideoPanel.Width, 0), new Size(recordingRectangle.Width - recordingVideoPanel.Width, recordingVideoPanel.Height));
+                FinishRecording();
+                recordingButton.RevertState();
+                recordingButton.Enabled = true;
+                optionsButton.Enabled = true;
             }
             else
             {
-                var screenBounds = ScreenHelper.GetScreenBounds();
-
-                if ((recordingRectangle.Location.Y + recordingVideoPanel.Height) < screenBounds.Height)
-                {
-                    Location = recordingRectangle.Location;
-                    Size = new Size(recordingRectangle.Width, recordingRectangle.Height + 1);
-                    excludeRectangle = new Rectangle(new Point(0, recordingVideoPanel.Height - 1), new Size(recordingRectangle.Width, recordingRectangle.Height - recordingVideoPanel.Height + 2)).OffsetRectangle(1);
-                    excludeRectangle2 = new Rectangle(new Point(recordingVideoPanel.Width, 1), new Size(recordingRectangle.Width - recordingVideoPanel.Width - 1, recordingVideoPanel.Height));
-                }
+                optionsButton.Enabled = false;
+                recordingTimer.Reset();
+                recorder = recordingService.GetRecorder();
+                StartWithDelay();
             }
-
-            RedShot.Platforms.Windows.WindowsRegionHelper.Exclude(this.ControlObject, excludeRectangle, excludeRectangle2);
-#endif
         }
 
-        private void RecordingVideoPanelClosed(object sender, EventArgs e)
+        private void StartWithDelay()
         {
+            int seconds = 3;
+            recordingButton.SetCountdownSecond(seconds);
+
+            var beforeRecordTimer = new UITimer()
+            {
+                Interval = 1
+            };
+            beforeRecordTimer.Elapsed += (o, e) =>
+            {
+                if (seconds != 1)
+                {
+                    seconds--;
+                    recordingButton.SetCountdownSecond(seconds);
+                }
+                else
+                {
+                    beforeRecordTimer.Stop();
+                    recorder.Start(recordingRectangle.OffsetRectangle(1));
+
+                    while (!recorder.IsRecording)
+                    {
+                    }
+
+                    recordingButton.SetCountdownSecond(0);
+                    recordingButton.RevertState();
+                    recordingTimer.Start();
+                    recordingButton.Enabled = true;
+                }
+            };
+            beforeRecordTimer.Start();
+        }
+
+        private void CloseButtonClicked(object sender, EventArgs e)
+        {
+            recorder?.Stop();
             Close();
+        }
+
+        private void FinishRecording()
+        {
+            recordingTimer.Stop();
+            recorder.Stop();
+
+            UploadingManager.RunUploading(recorder.GetVideo());
+        }
+
+        private void RenderingTimerElapsed(object sender, EventArgs e)
+        {
+            timerLabel.Text = recordingTimer.Elapsed.ToString(@"hh\:mm\:ss");
+            timerLabel.Invalidate();
+
+            if (recorder != null && recorder.IsRecording)
+            {
+                var rectangle = new Rectangle(Location, optionsPanelSize);
+                var mouseRectangle = new Rectangle((Point)Mouse.Position, new Size(1, 1));
+                if (rectangle.Intersects(mouseRectangle))
+                {
+                    Opacity = 1;
+                }
+                else
+                {
+                    Opacity = 0.001;
+                }
+            }
+            else
+            {
+                Opacity = 1;
+            }
+        }
+
+        private void OptionsButtonClicked(object sender, EventArgs e)
+        {
+            using var optionsDialog = new AudioOptionsDialog();
+            optionsDialog.ShowModal();
         }
     }
 }
---
+++ src/RedShot.Infrastructure.Recording/Views/RecordingView.eto.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using Eto.Drawing;
+using Eto.Forms;
+using RedShot.Resources;
+using RedShot.Infrastructure.Common;
+using RedShot.Infrastructure.Common.Forms;
+
+namespace RedShot.Infrastructure.Recording.Views
+{
+    /// <summary>
+    /// Recording view.
+    /// </summary>
+    internal partial class RecordingView : Form
+    {
+        private void InitializeComponents()
+        {
+            optionsPanelSize = new Size(220, 41);
+
+            recordingTimer = new Stopwatch();
+            renderingTimer = new UITimer
+            {
+                Interval = 0.01
+            };
+            renderingTimer.Elapsed += RenderingTimerElapsed;
+            renderingTimer.Start();
+
+            var recordingVideoPanel = GetOptionsPanel();
+
+            Content = new StackLayout()
+            {
+                Orientation = Orientation.Vertical,
+                Items =
+                {
+                    recordingVideoPanel
+                }
+            };
+
+#if _WINDOWS
+            WindowStyle = WindowStyle.None;
+            BackgroundColor = Colors.Red;
+
+            Rectangle excludeRectangle = default;
+            Rectangle excludeRectangle2 = default;
+
+            if ((recordingRectangle.Location.Y - recordingVideoPanel.Height) > 0)
+            {
+                Location = new Point(recordingRectangle.X, recordingRectangle.Y - recordingVideoPanel.Height - 1);
+                Size = new Size(recordingRectangle.Width, recordingRectangle.Height + recordingVideoPanel.Height + 1);
+                excludeRectangle = new Rectangle(new Point(0, recordingVideoPanel.Height), new Size(recordingRectangle.Width, recordingRectangle.Height + 1)).OffsetRectangle(1);
+                excludeRectangle2 = new Rectangle(new Point(recordingVideoPanel.Width, 0), new Size(recordingRectangle.Width - recordingVideoPanel.Width, recordingVideoPanel.Height));
+            }
+            else
+            {
+                var screenBounds = ScreenHelper.GetScreenBounds();
+
+                if ((recordingRectangle.Location.Y + recordingVideoPanel.Height) < screenBounds.Height)
+                {
+                    Location = recordingRectangle.Location;
+                    Size = new Size(recordingRectangle.Width, recordingRectangle.Height + 1);
+                    excludeRectangle = new Rectangle(new Point(0, recordingVideoPanel.Height - 1), new Size(recordingRectangle.Width, recordingRectangle.Height - recordingVideoPanel.Height + 2)).OffsetRectangle(1);
+                    excludeRectangle2 = new Rectangle(new Point(recordingVideoPanel.Width, 1), new Size(recordingRectangle.Width - recordingVideoPanel.Width - 1, recordingVideoPanel.Height));
+                }
+            }
+
+            RedShot.Platforms.Windows.WindowsRegionHelper.Exclude(this.ControlObject, excludeRectangle, excludeRectangle2);
+#endif
+        }
+
+        private Control GetOptionsPanel()
+        {
+            timerLabel = new Label()
+            {
+                Text = TimeSpan.Zero.ToString()
+            };
+            var buttonSize = new Size(40, 35);
+            var scaleSize = new Size(20, 18);
+
+            recordingButton = new RecordingButton(buttonSize);
+            recordingButton.Clicked += RecordingButtonClicked;
+
+            closeButton = new ImageButton(buttonSize, Icons.Close, scaleImageSize: scaleSize);
+            closeButton.ToolTip = "Close";
+            closeButton.Clicked += CloseButtonClicked;
+
+            optionsButton = new ImageButton(buttonSize, Icons.Gear, scaleImageSize: scaleSize);
+            optionsButton.ToolTip = "Audio Options";
+            optionsButton.Clicked += OptionsButtonClicked;
+
+            return new StackLayout()
+            {
+                Size = optionsPanelSize,
+                BackgroundColor = Colors.WhiteSmoke,
+                Orientation = Orientation.Horizontal,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Padding = 3,
+                Spacing = 5,
+                Items =
+                {
+                    recordingButton,
+                    timerLabel,
+                    FormsHelper.GetVoidBox(15),
+                    optionsButton,
+                    closeButton
+                }
+            };
+        }
+    }
+}
