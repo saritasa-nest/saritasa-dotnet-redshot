d3feb57675b20e37f4637e7a8d3601d6b3a9ce13
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-05 18:31:38 +0700

    Add query to get recording devices #SN-107

---
+++ version 2/src/RedShot.UseCases.Recording/GetRecordingDevices/GetRecordingDevicesQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using RedShot.Infrastructure.Domain.Recording.Devices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.UseCases.Recording.GetRecordingDevices
+{
+    public class GetRecordingDevicesQuery : IRequest<RecordingDevices>
+    {
+    }
+}
---
+++ version 2/src/RedShot.UseCases.Recording/GetRecordingDevices/GetRecordingDevicesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using RedShot.Infrastructure.Abstractions.Interfaces.Recording;
+using RedShot.Infrastructure.Domain.Recording.Devices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedShot.UseCases.Recording.GetRecordingDevices
+{
+    internal class GetRecordingDevicesQueryHandler : IRequestHandler<GetRecordingDevicesQuery, RecordingDevices>
+    {
+        private readonly IRecordingDevicesService recordingService;
+
+        public GetRecordingDevicesQueryHandler(IRecordingDevicesService recordingService)
+        {
+            this.recordingService = recordingService;
+        }
+
+        public async Task<RecordingDevices> Handle(GetRecordingDevicesQuery request, CancellationToken cancellationToken)
+        {
+            return await recordingService.GetRecordingDevicesAsync();
+        }
+    }
+}
