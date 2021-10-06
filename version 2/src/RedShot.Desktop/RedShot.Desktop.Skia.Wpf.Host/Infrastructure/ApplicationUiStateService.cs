385e989f606ef50e7bb31b1a4cdd10707b568c91
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-06 23:26:11 +0700

    Add application UI state service #SN-107

---
+++ version 2/src/RedShot.Desktop/RedShot.Desktop.Skia.Wpf.Host/Infrastructure/ApplicationUiStateService.cs
@@ -0,0 +1,39 @@
+﻿using RedShot.Mvvm.ServiceAbstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace RedShot.Desktop.Skia.Wpf.Host.Infrastructure
+{
+    internal class ApplicationUiStateService : IApplicationUiStateService
+    {
+        private readonly Window mainWindow;
+        private readonly Dispatcher dispatcher;
+
+        public ApplicationUiStateService(Window mainWindow, Dispatcher dispatcher)
+        {
+            this.mainWindow = mainWindow;
+            this.dispatcher = dispatcher;
+        }
+
+        public void HideUiInterface()
+        {
+            dispatcher.Invoke(() =>
+            {
+                mainWindow.Hide();
+                mainWindow.ShowInTaskbar = false;
+            }, DispatcherPriority.Loaded);
+        }
+
+        public void ShowUiInterface()
+        {
+            dispatcher.Invoke(() =>
+            {
+                mainWindow.ShowInTaskbar = true;
+                mainWindow.Activate();
+            });
+        }
+    }
+}
---
+++ version 2/src/RedShot.Mvvm/ServiceAbstractions/IApplicationUiStateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.Mvvm.ServiceAbstractions
+{
+    public interface IApplicationUiStateService
+    {
+        void HideUiInterface();
+
+        void ShowUiInterface();
+    }
+}
