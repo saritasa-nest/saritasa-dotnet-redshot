bf9fd85ee808049a07aa4649916d62812d3e7676
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-06 23:27:24 +0700

    Add implementation for encryption service #SN-107

---
+++ version 2/src/RedShot.Infrastructure.DomainServices/Services/Base64Encrypter.cs
@@ -0,0 +1,28 @@
+﻿using RedShot.Infrastructure.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.Infrastructure.DomainServices.Services
+{
+    /// <summary>
+    /// Base 64 implementation of encryption service. In general it doesn't provide any
+    /// security, the purpose is to no store plain text passwords.
+    /// </summary>
+    public class Base64Encrypter : IEncryptionService
+    {
+        /// <inheritdoc />
+        public string Decrypt(string value)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(value);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        /// <inheritdoc />
+        public string Encrypt(string value)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(value ?? string.Empty);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
