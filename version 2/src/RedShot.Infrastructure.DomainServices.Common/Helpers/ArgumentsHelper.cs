4c5e21dfc751af35a13249f31b6762e462ee7559
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-06 23:26:40 +0700

    Add helpers for recording services #SN-107

---
+++ version 2/src/RedShot.Infrastructure.DomainServices.Common/Helpers/ArgumentsHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.Infrastructure.DomainServices.Common.Helpers
+{
+    /// <summary>
+    /// Arguments helper.
+    /// </summary>
+    public static class ArgumentsHelper
+    {
+        /// <summary>
+        /// Split lines.
+        /// </summary>
+        public static IEnumerable<string> SplitLines(string text)
+        {
+            return text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+        }
+    }
+}
---
+++ version 2/src/RedShot.Infrastructure.DomainServices.Common/Helpers/UrlHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.Infrastructure.DomainServices.Common.Helpers
+{
+    /// <summary>
+    /// Helper for working with URL's and paths.
+    /// </summary>
+    public static class UrlHelper
+    {
+        /// <summary>
+        /// Combine two URLs.
+        /// </summary>
+        public static string CombineUrl(string url1, string url2)
+        {
+            var url1Empty = string.IsNullOrEmpty(url1);
+            var url2Empty = string.IsNullOrEmpty(url2);
+
+            if (url1Empty && url2Empty)
+            {
+                return "";
+            }
+
+            if (url1Empty)
+            {
+                return url2;
+            }
+
+            if (url2Empty)
+            {
+                return url1;
+            }
+
+            if (url1.EndsWith("/"))
+            {
+                url1 = url1[0..^1];
+            }
+
+            if (url2.StartsWith("/"))
+            {
+                url2 = url2.Remove(0, 1);
+            }
+
+            return url1 + "/" + url2;
+        }
+
+        /// <summary>
+        /// Gives directory path.
+        /// </summary>
+        public static string GetDirectoryPath(string path)
+        {
+            if (path.Contains("/"))
+            {
+                path = path.Substring(0, path.LastIndexOf('/'));
+            }
+
+            return path;
+        }
+
+        /// <summary>
+        /// Get all paths from path string.
+        /// </summary>
+        public static List<string> GetPaths(string path)
+        {
+            List<string> paths = new List<string>();
+
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == '/')
+                {
+                    string currentPath = path.Remove(i);
+
+                    if (!string.IsNullOrEmpty(currentPath))
+                    {
+                        paths.Add(currentPath);
+                    }
+                }
+                else if (i == path.Length - 1)
+                {
+                    paths.Add(path);
+                }
+            }
+
+            return paths;
+        }
+    }
+}
