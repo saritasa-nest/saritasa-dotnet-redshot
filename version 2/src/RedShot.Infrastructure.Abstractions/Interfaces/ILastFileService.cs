9dd7af3bbb538fc7ca963d23b6fa4af98c97d544
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-07 19:07:20 +0700

    Add last file service #SN-107

---
+++ version 2/src/RedShot.Infrastructure.Abstractions/Interfaces/ILastFileService.cs
@@ -0,0 +1,14 @@
+﻿using RedShot.Infrastructure.Domain.Files;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.Infrastructure.Abstractions.Interfaces
+{
+    public interface ILastFileService
+    {
+        IObservable<File> LastFileNotification { get; }
+
+        void SetLastFile(File file);
+    }
+}
---
+++ version 2/src/RedShot.Infrastructure.DomainServices/Services/LastFileService.cs
@@ -0,0 +1,26 @@
+﻿using RedShot.Infrastructure.Abstractions.Interfaces;
+using RedShot.Infrastructure.Domain.Files;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Subjects;
+using System.Text;
+
+namespace RedShot.Infrastructure.DomainServices.Services
+{
+    internal class LastFileService : ILastFileService
+    {
+        private readonly ReplaySubject<File> lastFileSubject;
+
+        public LastFileService()
+        {
+            lastFileSubject = new ReplaySubject<File>(1);
+        }
+
+        public IObservable<File> LastFileNotification => lastFileSubject;
+
+        public void SetLastFile(File file)
+        {
+            lastFileSubject.OnNext(file);
+        }
+    }
+}
