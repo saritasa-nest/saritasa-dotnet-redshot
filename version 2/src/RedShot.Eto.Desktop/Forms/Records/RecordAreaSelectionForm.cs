82b4a697cfbca65195d9d090b57cd1c09c5be3bf
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-07 19:08:49 +0700

    Add record area selection form and presenter #SN-107

---
+++ version 2/src/RedShot.Eto.Desktop/Forms/Records/RecordAreaSelectionForm.cs
@@ -0,0 +1,26 @@
+﻿using RedShot.Eto.Desktop.Forms.Common;
+using RedShot.Eto.Mvp.Presenters.Records;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace RedShot.Eto.Desktop.Forms.Records
+{
+    public class RecordAreaSelectionForm : SelectionFormBase, IRecordAreaSelectionView
+    {
+        /// <inheritdoc/>
+        protected override string TopMessage { get; set; } = "Please select a region to record";
+
+        public event EventHandler<Rectangle> SelectionUpdated;
+
+        /// <inheritdoc/>
+        protected override void FinishSelection()
+        {
+            var selectionArea = GetRealSelectionRegion();
+            var systemFormatArea = new System.Drawing.Rectangle(selectionArea.X, selectionArea.Y, selectionArea.Width, selectionArea.Height);
+            SelectionUpdated?.Invoke(this, systemFormatArea);
+            CallReadyToClose();
+        }
+    }
+}
---
+++ version 2/src/RedShot.Eto.Mvp/Presenters/Records/RecordAreaSelectionPresenter.cs
@@ -0,0 +1,66 @@
+﻿using RedShot.Desktop.Infrastructure.Common.Navigation;
+using RedShot.Eto.Mvp.Presenters.Common;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedShot.Eto.Mvp.Presenters.Records
+{
+    public interface IRecordAreaSelectionView : IView
+    {
+        event EventHandler<Rectangle> SelectionUpdated;
+
+        event EventHandler ScreenChanged;
+    }
+
+    public enum SelectionStatus
+    {
+        Canceled,
+        NeedToChangeScreen,
+        Selected
+    }
+
+    public struct SelectionResult
+    {
+        public SelectionStatus Status { get; set; }
+
+        public Rectangle SelectionArea { get; set; }
+    }
+
+    public class RecordAreaSelectionPresenter : BasePresenter<IRecordAreaSelectionView>, IWithResult<SelectionResult>
+    {
+        public SelectionResult Result { get; private set; }
+
+        public override async Task InitializePresenterAsync(IRecordAreaSelectionView view)
+        {
+            await base.InitializePresenterAsync(view);
+            view.SelectionUpdated += ViewSelectionUpdated;
+            view.ScreenChanged += ViewScreenChanged;
+            view.FormReadyToClose += ViewFormReadyToClose;
+        }
+
+        private void ViewSelectionUpdated(object sender, Rectangle area)
+        {
+            Result = new SelectionResult()
+            {
+                SelectionArea = area,
+                Status = SelectionStatus.Selected
+            };
+        }
+
+        private void ViewFormReadyToClose(object sender, EventArgs e)
+        {
+            View.Close();
+        }
+
+        private void ViewScreenChanged(object sender, EventArgs e)
+        {
+            Result = new SelectionResult()
+            {
+                Status = SelectionStatus.NeedToChangeScreen
+            };
+        }
+    }
+}
