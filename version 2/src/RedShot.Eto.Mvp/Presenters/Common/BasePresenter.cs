a585f3b1d72bff12cd7272c8e7c375da886062de
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-07 19:09:48 +0700

    Rework presenters structure #SN-107

--- version 2/src/RedShot.Eto.Mvp/Presenters/Common/BasePresenter.cs
+++ version 2/src/RedShot.Eto.Mvp/Presenters/Common/BasePresenter.cs
@@ -1,4 +1,5 @@
 ﻿using Eto.Forms;
+using System;
 using System.Threading.Tasks;
 
 namespace RedShot.Eto.Mvp.Presenters.Common
@@ -6,7 +7,12 @@ namespace RedShot.Eto.Mvp.Presenters.Common
     /// <summary>
     /// View abstraction for presenter.
     /// </summary>
-    public interface IView { };
+    public interface IView
+    {
+        event EventHandler<EventArgs> FormReadyToClose;
+
+        void Close();
+    }
 
     /// <summary>
     /// Base presenter class.
@@ -15,12 +21,15 @@ namespace RedShot.Eto.Mvp.Presenters.Common
     public abstract class BasePresenter<T>
         where T : IView
     {
+        protected T View { get; private set; }
+
         /// <summary>
         /// Initialize presenter.
         /// </summary>
         /// <param name="view">View abstraction.</param>
         public virtual Task InitializePresenterAsync(T view)
         {
+            View = view;
             return Task.CompletedTask;
         }
     }
--- version 2/src/RedShot.Eto.Mvp/Presenters/ScreenshotsPresenter.cs
+++ version 2/src/RedShot.Eto.Mvp/Presenters/ScreenshotsPresenter.cs
@@ -1,9 +1,9 @@
 ﻿using Eto.Drawing;
 using RedShot.Desktop.Infrastructure.Common.Navigation;
 using RedShot.Eto.Mvp.Presenters.Common;
+using RedShot.Infrastructure.Domain.Files;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,21 +12,57 @@ namespace RedShot.Eto.Mvp.Presenters
     public interface IScreenshotsView : IView
     {
         event EventHandler<Bitmap> ScreenshotUpdated;
+
+        event EventHandler ScreenChanged;
     }
 
-    public class ScreenshotsPresenter : BasePresenter<IScreenshotsView>, IWithResult<byte[]>
+    public enum ScreenshotStatus
     {
-        public byte[] Result { get; private set; }
+        Canceled,
+        NeedToChangeScreen,
+        Captured
+    }
+
+    public struct ScreenshotResult
+    {
+        public ScreenshotStatus Status { get; set; }
+
+        public byte[] Screenshot { get; set; }
+    }
+
+    public class ScreenshotsPresenter : BasePresenter<IScreenshotsView>, IWithResult<ScreenshotResult>
+    {
+        public ScreenshotResult Result { get; private set; }
 
         public override async Task InitializePresenterAsync(IScreenshotsView view)
         {
             await base.InitializePresenterAsync(view);
             view.ScreenshotUpdated += ViewScreenshotUpdated;
+            view.ScreenChanged += ViewScreenChanged;
+            view.FormReadyToClose += ViewFormReadyToClose;
+        }
+
+        private void ViewFormReadyToClose(object sender, EventArgs e)
+        {
+            View.Close();
+        }
+
+        private void ViewScreenChanged(object sender, EventArgs e)
+        {
+            Result = new ScreenshotResult()
+            {
+                Status = ScreenshotStatus.NeedToChangeScreen
+            };
         }
 
         private void ViewScreenshotUpdated(object sender, Bitmap screenshot)
         {
-            Result = screenshot.ToByteArray(ImageFormat.Bitmap);
+            var screenshotBytes = screenshot.ToByteArray(ImageFormat.Bitmap);
+            Result = new ScreenshotResult()
+            {
+                Screenshot = screenshotBytes,
+                Status = ScreenshotStatus.Captured
+            };
         }
     }
 }
