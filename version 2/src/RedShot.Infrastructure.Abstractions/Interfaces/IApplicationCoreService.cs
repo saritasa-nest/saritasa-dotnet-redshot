95fed3323e9aa721547f95f9e400f139b4e21ca9
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-07 19:12:57 +0700

    Add application core service #SN-107

---
+++ version 2/src/RedShot.Infrastructure.Abstractions/Interfaces/IApplicationCoreService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedShot.Infrastructure.Abstractions.Interfaces
+{
+    public interface IApplicationCoreService
+    {
+        Task TakeScreenshotAsync();
+
+        Task RecordVideoAsync();
+
+        Task OpenConfigurationAsync();
+
+        Task OpenLastFileAsync();
+
+        Task SendFeedBackAsync();
+
+        void CloseApplication();
+    }
+}
---
+++ version 2/src/RedShot.Infrastructure.DomainServices/Services/ApplicationCoreService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Options;
+using RedShot.Infrastructure.Abstractions.Interfaces;
+using RedShot.Infrastructure.Domain.Files;
+using RedShot.Infrastructure.Domain.Settings;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedShot.Infrastructure.DomainServices.Services
+{
+    internal class ApplicationCoreService : IApplicationCoreService
+    {
+        private readonly IEtoFormsBridgeService etoFormsBridgeService;
+        private readonly IUnoPlatformBridgeService unoPlatformBridgeService;
+        private readonly IApplicationStateService applicationStateService;
+        private readonly ILastFileService lastFileService;
+        private readonly ApplicationSettings applicationSettings;
+
+        private File lastFile;
+
+        public ApplicationCoreService(
+            IEtoFormsBridgeService etoFormsBridgeService,
+            IUnoPlatformBridgeService unoPlatformBridgeService,
+            IApplicationStateService applicationStateService,
+            ILastFileService lastFileService,
+            IOptions<ApplicationSettings> applicationSettings)
+        {
+            this.etoFormsBridgeService = etoFormsBridgeService;
+            this.unoPlatformBridgeService = unoPlatformBridgeService;
+            this.applicationStateService = applicationStateService;
+            this.lastFileService = lastFileService;
+            this.applicationSettings = applicationSettings.Value;
+        }
+
+        public void CloseApplication()
+        {
+            applicationStateService.ShutdownApplication();
+        }
+
+        public async Task OpenConfigurationAsync()
+        {
+            await unoPlatformBridgeService.OpenConfigurationViewAsync();
+        }
+
+        public Task OpenLastFileAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RecordVideoAsync()
+        {
+            var recordingArea = await etoFormsBridgeService.OpenRecordingAreaSelectionForm();
+            var video = await etoFormsBridgeService.OpenRecordingForm(recordingArea);
+
+            lastFile = video;
+            lastFileService.SetLastFile(video);
+        }
+
+        public async Task SendFeedBackAsync()
+        {
+            var url = $"mailto:{applicationSettings.DeveloperEmail}";
+
+            await Task.Run(() =>
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            });
+        }
+
+        public async Task TakeScreenshotAsync()
+        {
+            var screenshot = await etoFormsBridgeService.OpenScreenshotForm();
+
+            lastFile = screenshot;
+            lastFileService.SetLastFile(screenshot);
+
+            await unoPlatformBridgeService.OpenScreenshotViewAsync(screenshot);
+        }
+    }
+}
