51567846ff8556a2cb25ba29148dd5af16d7715f
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-07 19:07:58 +0700

    Add application state service for windows #SN-107

---
+++ version 2/src/RedShot.Desktop/RedShot.Desktop.Skia.Wpf.Host/Infrastructure/ApplicationStateService.cs
@@ -0,0 +1,26 @@
+﻿using RedShot.Infrastructure.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace RedShot.Desktop.Skia.Wpf.Host.Infrastructure
+{
+    internal class ApplicationStateService : IApplicationStateService
+    {
+        private readonly Application application;
+
+        public ApplicationStateService(Application application)
+        {
+            this.application = application;
+        }
+
+        public void ShutdownApplication()
+        {
+            application.Dispatcher.Invoke(() =>
+            {
+                application.Shutdown();
+            });
+        }
+    }
+}
