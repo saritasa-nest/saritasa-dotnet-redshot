0c8f1cd766e5f17385460b1bfae4c8c6e69e5c74
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-07 19:06:45 +0700

    Add UNO platform bridge service #SN-107

---
+++ version 2/src/RedShot.Infrastructure.Abstractions/Interfaces/IUnoPlatformBridgeService.cs
@@ -0,0 +1,15 @@
+﻿using RedShot.Infrastructure.Domain.Files;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedShot.Infrastructure.Abstractions.Interfaces
+{
+    public interface IUnoPlatformBridgeService
+    {
+        Task OpenConfigurationViewAsync();
+
+        Task OpenScreenshotViewAsync(File screenshot);
+    }
+}
---
+++ version 2/src/RedShot.Mvvm/Infrastructure/UnoPlatformBridgeService.cs
@@ -0,0 +1,22 @@
+﻿using RedShot.Infrastructure.Abstractions.Interfaces;
+using RedShot.Infrastructure.Domain.Files;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedShot.Mvvm.Infrastructure
+{
+    internal class UnoPlatformBridgeService : IUnoPlatformBridgeService
+    {
+        public Task OpenConfigurationViewAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OpenScreenshotViewAsync(File screenshot)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
