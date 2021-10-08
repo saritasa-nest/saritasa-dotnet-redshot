769fc6c0fd57e7ee7818abe4a61f16520aabf595
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-08 19:46:53 +0700

    Add clipboard service #SN-107

---
+++ version 2/src/RedShot.Eto.Desktop/Infrastructure/EtoClipboardService.cs
@@ -0,0 +1,17 @@
+﻿using Eto.Forms;
+using RedShot.Infrastructure.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.Eto.Desktop.Infrastructure
+{
+    internal class EtoClipboardService : IClipboardService
+    {
+        public void SaveToClipboard(string text)
+        {
+            Clipboard.Instance.Clear();
+            Clipboard.Instance.Text = text;
+        }
+    }
+}
