bf29335c2ac59f110f1801e6e49ecca6174bbb4a
Pavel Terentev <pavel.terentyev@saritasa.com>
2021-10-08 19:47:48 +0700

    Add upload to FTP command #SN-107

---
+++ version 2/src/RedShot.UseCases/UploadToFtp/UploadToFtpCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using RedShot.Infrastructure.Configuration.Models.Account;
+using RedShot.Infrastructure.Domain.Files;
+using RedShot.Infrastructure.Domain.Ftp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedShot.UseCases.UploadToFtp
+{
+    public class UploadToFtpCommand : IRequest
+    {
+        public FtpAccount FtpAccount { get; set; }
+
+        public File FileToUpload { get; set; }
+    }
+}
---
+++ version 2/src/RedShot.UseCases/UploadToFtp/UploadToFtpCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using RedShot.Infrastructure.Abstractions.Interfaces.Ftp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedShot.UseCases.UploadToFtp
+{
+    internal class UploadToFtpCommandHandler : IRequestHandler<UploadToFtpCommand>
+    {
+        private readonly IFtpClientFactory ftpClientFactory;
+
+        public UploadToFtpCommandHandler(IFtpClientFactory ftpClientFactory)
+        {
+            this.ftpClientFactory = ftpClientFactory;
+        }
+
+        public async Task<Unit> Handle(UploadToFtpCommand request, CancellationToken cancellationToken)
+        {
+            using var client = ftpClientFactory.GetFtpClient(request.FtpAccount);
+            var fileUrl = await client.UploadAsync(request.FileToUpload, cancellationToken);
+        }
+    }
+}
